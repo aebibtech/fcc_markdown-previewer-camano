b168fbb30d73defc8ac46f3d4301eb909f0b3011
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-03-28 00:48:49 +0800

    iframes for proper html display on preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,6 +3,7 @@ export default function Preview({htmlContent}){
         <fieldset>
             <legend>Preview area</legend>
             <div dangerouslySetInnerHTML={{ __html: htmlContent }} id="preview"></div>
+            <iframe srcdoc={htmlContent} id="preview-show"></iframe>
         </fieldset>
     )
 }
