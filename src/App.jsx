0f7971831e46fc028904dcdf39dfe7310921a8dd
Paul Abib S. Camano <paul.camano@gmail.com>
2024-11-21 16:29:25 +0000

    refactor state

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import Editor from './components/Editor'
 import Preview from './components/Preview'
 
 export default function App() {
-	const [markdown, setMarkDown] = useState("")
-	const [html, setHTML] = useState("")
+	const [appState, setAppState] = useState({ markdown: '', html: '' })
 	const startHead = '<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'
 	const styleSheet = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">\n`
 	const endHead = '</head>\n'
@@ -18,12 +17,10 @@ export default function App() {
 		(async function(){
 			try{
 				const content = await axios.get('/sample.md')
-				setMarkDown(content.data)
-				setHTML(() => `${startHead}${styleSheet}${endHead}${startBody}${marked.parse(content.data)}${endBody}`)
+				setAppState(() => ({ markdown: content.data, html: `${startHead}${styleSheet}${endHead}${startBody}${marked.parse(content.data)}${endBody}` }))
 			}catch(e){
 				const errMsg = "No data received."
-				setMarkDown(() => errMsg)
-				setHTML(() => errMsg)
+				setAppState(() => ({ markdown: errMsg, html: errMsg }))
 			}
 			marked.setOptions({
 				breaks: true
@@ -36,52 +33,51 @@ export default function App() {
 			const data = new Blob([text], { type: 'text/plain' })
 			return window.URL.createObjectURL(data)
 		}
-		return "javascript:;";
+		return "javascript:;"
 	}
 
 	function onEditorChange(event){
-		setMarkDown(() => event.target.value)
-		setHTML(() => `${startHead}${styleSheet}${endHead}${startBody}${marked.parse(event.target.value)}${endBody}`)
+		setAppState(() => ({ markdown: event.target.value, html: `${startHead}${styleSheet}${endHead}${startBody}${marked.parse(event.target.value)}${endBody}` }))
 	}
 
 	function onDownload(event){
 		const link = document.createElement('a')
 		link.setAttribute('download', 'markdown.md')
-		link.href = makeTextFile(markdown)
+		link.href = makeTextFile(appState.markdown)
 		document.body.appendChild(link)
 	
 		window.requestAnimationFrame(function () {
 			const event = new MouseEvent('click')
 			link.dispatchEvent(event)
 			document.body.removeChild(link)
-		});
+		})
 	}
 
 	function onDownloadHTML(event){
 		const link = document.createElement('a')
 		link.setAttribute('download', 'output.html')
-		link.href = makeTextFile(html)
+		link.href = makeTextFile(appState.html)
 		document.body.appendChild(link)
 	
 		window.requestAnimationFrame(function () {
 			const event = new MouseEvent('click')
 			link.dispatchEvent(event)
 			document.body.removeChild(link)
-		});
+		})
 	}
 
 	async function onCopy(event){
-		await navigator.clipboard.writeText(markdown)
+		await navigator.clipboard.writeText(appState.markdown)
 	}
 
 	return(
 		<>
 		<div className="row gap-0 mb-4" id="previewer">
 			<div className="col-12 col-sm-12 col-md-12 col-lg-6 mb-5">
-				<Editor handler={onEditorChange} markdown={markdown} />
+				<Editor handler={onEditorChange} markdown={appState.markdown} />
 			</div>
 			<div className="col-12 col-sm-12 col-md-12 col-lg-6 mb-5">
-				<Preview htmlContent={html} />
+				<Preview htmlContent={appState.html} />
 			</div>
 		</div>
 		<div className="row justify-content-center align-items-center mb-2">
